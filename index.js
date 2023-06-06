733434f77413e45f13ce1ae427d3cf580ede0b96
MadinkuM <135757318+MadinkuM@users.noreply.github.com>
2023-06-06 22:55:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector("#navbar").innerHTML=`<header>
+<img class="logo" src="./Images/logo.png" alt="logo">
+<nav>
+    <ul class="nav-links">
+        <li>
+            <a href="index.html">Home</a>
+        </li>
+        <li>
+            <a href="about.html">About</a>
+        </li>
+        <li>
+            <a href="skills.html">Skills</a>
+        </li>
+        <li>
+            <a href="contacts.html">Contact</a>
+        </li>
+    
+
+    </ul>
+</nav>
+</header>`;
+
