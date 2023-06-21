230e35cabaf2b0d6253901d203b4ed84a4314cc8
madinku <madinku@technanimals.com>
2023-06-21 20:21:05 +0200

    add modification to website

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 document.querySelector("#navbar").innerHTML=`<header>
 <img class="logo" src="./Images/logo.png" alt="logo">
 <nav>
-    <ul class="nav-links">
-        <li>
-            <a href="index.html">Home</a>
+    <ul class="nav-menu">
+        <li class= "nav-item">
+            <a href="index.html" class="nav-link" >Home</a>
         </li>
-        <li>
-            <a href="about.html">About</a>
+        <li class= "nav-item">
+            <a href="about.html" class="nav-link" >About</a>
         </li>
-        <li>
-            <a href="skills.html">Skills</a>
+        <li class= "nav-item">
+            <a href="skills.html" class="nav-link" >Skills</a>
         </li>
-        <li>
-            <a href="contacts.html">Contact</a>
+        <li class= "nav-item">
+            <a href="contacts.html" class="nav-link" >Contact</a>
         </li>
-    
-
     </ul>
+    <div class="hamburger">
+    <span class="line"></span>
+    <span class="line"></span>
+    <span class="line"></span>
+</div>
 </nav>
 </header>`;
+const hamburger =document.querySelector(".hamburger");
+const navMenu=document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 
